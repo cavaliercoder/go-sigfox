1bf665910128562e76b1688c2a16d176f71cfe2f
Davor Kapsa <davor.kapsa@gmail.com>
2018-10-18 13:40:14 +0200

    clean callback ServeHTTP

--- callback_handler.go
+++ callback_handler.go
@@ -12,17 +12,14 @@ type callbackHandler interface {
 // handler for a SIGFOX callback base type.
 type callbackHandlerFunc func(*callback)
 
+// ServeHTTP serves callback HTTP response.
 func (f callbackHandlerFunc) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	status := http.StatusInternalServerError
-
-	// parse request body
 	cb := &callback{}
+	status := http.StatusNoContent
 	if s, err := parseCallback(r, cb); err != nil {
 		// TODO: make error available to user
-		dprintf("Error parsing callbackL %v", err)
-		status = s
-	} else {
-		status = http.StatusNoContent
+		dprintf("Error parsing callback %v", err)
+		status = http.StatusInternalServerError
 	}
 
 	// respond to sigfox server
